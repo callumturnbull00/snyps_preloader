f0f3e14a4803c2a25b78fcf6d65bca7954ba2e68
callumturnbull00 <callumturnbull00@gmail.com>
2024-09-23 18:29:15 +0100

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/callumturnbull00/snyps_preloader/preload.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/callumturnbull00/snyps_preloader/bouncing-ball.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
